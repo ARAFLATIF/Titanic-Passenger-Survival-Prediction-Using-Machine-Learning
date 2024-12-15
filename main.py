5a53dde85755988562cb38d9fe34094102bd2cfb
Araf Ibne Latif <124219901+ARAFLATIF@users.noreply.github.com>
2024-12-14 19:56:03 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from model import TitanicModel
-import os
 
 app = Flask(__name__)
 
@@ -13,19 +12,17 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    if request.method == 'POST':
-        features = {
-            'Pclass': int(request.form['Pclass']),
-            'Sex': request.form['Sex'],
-            'Age': float(request.form['Age']),
-            'Fare': float(request.form['Fare']),
-            'Embarked': request.form['Embarked'],
-            'FamilySize': int(request.form['FamilySize'])
-        }
-        prediction = model.predict(features)
-        survival = 'Survived' if prediction[0] == 1 else 'Did Not Survive'
-        return render_template('index.html', prediction=survival)
+    features = {
+        'Pclass': int(request.form['Pclass']),
+        'Sex': request.form['Sex'],
+        'Age': float(request.form['Age']),
+        'Fare': float(request.form['Fare']),
+        'Embarked': request.form['Embarked'],
+        'FamilySize': int(request.form['FamilySize'])
+    }
+    prediction = model.predict(features)
+    survival = 'Survived' if prediction[0] == 1 else 'Did Not Survive'
+    return jsonify({'prediction': survival})
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
