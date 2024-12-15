a4113215afca3dc6c158ab5392c35157a0391db6
Araf Ibne Latif <124219901+ARAFLATIF@users.noreply.github.com>
2024-12-14 19:56:26 -0600

    Update model.py

--- model.py
+++ model.py
@@ -1,7 +1,6 @@
 import pandas as pd
-import numpy as np
-from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler, LabelEncoder
 
 class TitanicModel:
     def __init__(self):
@@ -12,24 +11,16 @@ class TitanicModel:
 
     def train_model(self, url):
         data = pd.read_csv(url)
-        data.drop(columns=['PassengerId', 'Name', 'Ticket', 'Cabin'], inplace=True)
-        data['Age'].fillna(data['Age'].median(), inplace=True)
-        data['Embarked'].fillna(data['Embarked'].mode()[0], inplace=True)
-        data['Sex'] = self.label_encoder_sex.fit_transform(data['Sex'])
-        data['Embarked'] = self.label_encoder_embarked.fit_transform(data['Embarked'])
-        data['FamilySize'] = data['SibSp'] + data['Parch'] + 1
-        data.drop(columns=['SibSp', 'Parch'], inplace=True)
-
+        # Data preprocessing steps here
         X = data.drop(columns=['Survived'])
         y = data['Survived']
         X = self.scaler.fit_transform(X)
-
         self.model = RandomForestClassifier(n_estimators=200, max_depth=10, random_state=42)
         self.model.fit(X, y)
 
     def predict(self, input_data):
         input_df = pd.DataFrame([input_data])
-        input_df['Sex'] = self.label_encoder_sex.transform(input_df['Sex'])
-        input_df['Embarked'] = self.label_encoder_embarked.transform(input_df['Embarked'])
+        # Preprocess input data
         input_df = self.scaler.transform(input_df)
         return self.model.predict(input_df)
+
