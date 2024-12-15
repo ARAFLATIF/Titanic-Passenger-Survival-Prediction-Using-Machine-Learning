1b2571ce1ab6c5bd4bc4657a677495c390d63448
Araf Ibne Latif <124219901+ARAFLATIF@users.noreply.github.com>
2024-12-14 19:13:54 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from model import TitanicModel
+import os
 
 app = Flask(__name__)
 
@@ -26,4 +27,6 @@ def predict():
         return render_template('index.html', prediction=survival)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
